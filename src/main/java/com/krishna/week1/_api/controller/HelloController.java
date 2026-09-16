421fed667c7f72d405611cc7c8cf22134593414d
athira <asuriyakrishna@my365.bellevue.edu>
2026-09-15 22:51:24 -0500

    add citation

--- src/main/java/com/krishna/week1/_api/controller/HelloController.java
+++ src/main/java/com/krishna/week1/_api/controller/HelloController.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University,
+ * all rights reserved.
+ */
 @RestController
 public class HelloController {
 
